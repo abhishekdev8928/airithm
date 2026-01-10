7ee1ede09d7687e028a5301ea8f15c7939d2c6e7
Devendramali <devendramali01234@gmail.com>
2026-01-10 20:29:46 +0530

    red

--- src/components/card/automation/LifeCycleCard.jsx
+++ src/components/card/automation/LifeCycleCard.jsx
@@ -1,14 +1,13 @@
-
 import * as LucideIcon from "lucide-react";
 
 
 function LifeCycleCard({ title, description, icon, iconpath }) {
   const IconComponet = LucideIcon[icon];
   return (
-    <div className="lifecycle-card lg:max-w-[300px] max-w-[100%]  sm:w-[100%] bg-[linear-gradient(180deg,rgba(255,255,255,0.1)_0%,rgba(153,153,153,0.1)_100%)] p-[16px] rounded-2xl">
-      <div className="size-[24px] flex justify-center items-center rounded-sm ">
+    <div className="lifecycle-card w-full sm:max-w-[300px]  bg-[linear-gradient(180deg,rgba(255,255,255,0.1)_0%,rgba(153,153,153,0.1)_100%)] p-[16px] rounded-2xl">
+      <div className="size-6 flex justify-center items-center rounded-sm ">
         {/* <IconComponet size="32" className="text-[#C44558]" /> */}
-        <img src={iconpath} alt="" className="h-[24px]" />
+        <img src={iconpath} alt="" className="h-6" />
       </div>
 
       <div className="lifecycle-card-content pt-[24px] space-y-4 leading-[1.4] text-white">
